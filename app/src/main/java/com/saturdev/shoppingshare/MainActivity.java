a51a535b9b4fbdf97af6b54e2e5cea4e20ea0645
Brian Weloba <bweloba@gmail.com>
2021-05-20 17:47:12 +0300

    Add button and link it to new view

--- app/src/main/java/com/saturdev/shoppingshare/MainActivity.java
+++ app/src/main/java/com/saturdev/shoppingshare/MainActivity.java
@@ -2,7 +2,11 @@ package com.saturdev.shoppingshare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mContinueButton = (Button) findViewById(R.id.continueButton);
+
+        mContinueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,WelcomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
