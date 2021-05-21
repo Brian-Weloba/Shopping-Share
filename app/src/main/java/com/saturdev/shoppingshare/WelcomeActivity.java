7e952db0959147da42cff80243741704a40597fb
Brian Weloba <bweloba@gmail.com>
2021-05-21 11:47:21 +0300

    Add link and onclick listener to new view

--- app/src/main/java/com/saturdev/shoppingshare/WelcomeActivity.java
+++ app/src/main/java/com/saturdev/shoppingshare/WelcomeActivity.java
@@ -2,7 +2,10 @@ package com.saturdev.shoppingshare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Button mContButton = (Button) findViewById(R.id.cont_button);
+
+        mContButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this,Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
