e585acd6766e8ced583dab2de793d99e52bcae14
Brian Weloba <bweloba@gmail.com>
2021-05-27 18:44:40 +0300

    Add intent to next activity and use butterknife

--- app/src/main/java/com/saturdev/shoppingshare/WelcomeActivity.java
+++ app/src/main/java/com/saturdev/shoppingshare/WelcomeActivity.java
@@ -7,19 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class WelcomeActivity extends AppCompatActivity {
+    @BindView(R.id.cont_button) Button mContButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-
-        Button mContButton = (Button) findViewById(R.id.cont_button);
+        ButterKnife.bind(this);
 
         mContButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(WelcomeActivity.this,Login.class);
+                Intent intent = new Intent(WelcomeActivity.this,SignUp.class);
                 startActivity(intent);
             }
         });
