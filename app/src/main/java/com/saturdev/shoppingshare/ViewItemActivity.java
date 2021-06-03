77a33108556792cd5e76ade3bdbf7b1fd8e7ddbe
Brian Weloba <bweloba@gmail.com>
2021-06-03 13:05:26 +0300

    Add page to view individual items

---
+++ app/src/main/java/com/saturdev/shoppingshare/ViewItemActivity.java
@@ -0,0 +1,14 @@
+package com.saturdev.shoppingshare;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_item);
+    }
+}
