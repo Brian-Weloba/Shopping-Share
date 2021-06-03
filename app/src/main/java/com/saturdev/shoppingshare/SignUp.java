212fe40c38ed3cfd043598f010774efd05e89a7a
Brian Weloba <bweloba@gmail.com>
2021-06-03 13:16:30 +0300

    Add function to log in previous users automatically

--- app/src/main/java/com/saturdev/shoppingshare/SignUp.java
+++ app/src/main/java/com/saturdev/shoppingshare/SignUp.java
@@ -2,6 +2,7 @@ package com.saturdev.shoppingshare;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -25,33 +26,51 @@ public class SignUp extends AppCompatActivity {
     @BindView(R.id.buttonSignUp)
     Button mSignUp;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         ButterKnife.bind(this);
 
+        SharedPreferences sharedPref = getSharedPreferences("data", MODE_PRIVATE);
+        int number = sharedPref.getInt("isLogged", 0);
+
         mSpinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, CountryData.countryNames));
 
-        mSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String code = CountryData.countryAreaCodes[mSpinner.getSelectedItemPosition()];
-                String number = mPhoneNumber.getText().toString().trim();
+        if (number == 0) {
+            mSignUp.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String code = CountryData.countryAreaCodes[mSpinner.getSelectedItemPosition()];
+                    String number = mPhoneNumber.getText().toString().trim();
 
-                if (number.isEmpty() || number.length() < 9) {
-                    mPhoneNumber.setError("Valid number is required");
-                    mPhoneNumber.requestFocus();
-                    return;
+                    if (number.isEmpty() || number.length() < 9) {
+                        mPhoneNumber.setError("Valid number is required");
+                        mPhoneNumber.requestFocus();
+                        return;
+                    }
+
+                    String phoneNum = "+" + code + number;
+
+                    Intent intent = new Intent(SignUp.this, VerifyPhoneActivity.class);
+                    intent.putExtra("phonenumber", phoneNum);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
                 }
+            });
 
-                String phoneNum = "+" + code + number;
 
-                Intent intent = new Intent(SignUp.this, VerifyPhoneActivity.class);
-                intent.putExtra("phonenumber", phoneNum);
-                startActivity(intent);
-            }
-        });
+            SharedPreferences.Editor prefEditor = sharedPref.edit();
+            prefEditor.putInt("isLogged", 1);
+            prefEditor.apply();
+        } else {
+            Intent intent = new Intent(SignUp.this, HomeActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
 
     }
 }
