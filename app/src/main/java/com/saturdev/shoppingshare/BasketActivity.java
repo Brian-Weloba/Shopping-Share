44ef87a115722ef7d3c264ba4eed8acc04893f4a
Brian Weloba <bweloba@gmail.com>
2021-06-03 13:03:52 +0300

    Add activity to display basket

---
+++ app/src/main/java/com/saturdev/shoppingshare/BasketActivity.java
@@ -0,0 +1,14 @@
+package com.saturdev.shoppingshare;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BasketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basket);
+    }
+}
