56c726a048f29fca542950b49550fe7de5ff3ef8
Brian Weloba <bweloba@gmail.com>
2021-06-03 13:17:01 +0300

    Add class for users

---
+++ app/src/main/java/com/saturdev/shoppingshare/users/Users.java
@@ -0,0 +1,43 @@
+package com.saturdev.shoppingshare.users;
+
+public class Users {
+    int userId;
+    String name;
+    String phone;
+    String email;
+
+    public Users() {
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
