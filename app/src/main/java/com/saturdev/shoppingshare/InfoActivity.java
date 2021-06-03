bd746f5bcf9220fb974f17d32810a88a445130c7
Brian Weloba <bweloba@gmail.com>
2021-06-03 13:20:19 +0300

    Update system files

--- app/src/main/java/com/saturdev/shoppingshare/InfoActivity.java
+++ app/src/main/java/com/saturdev/shoppingshare/InfoActivity.java
@@ -1,26 +1,35 @@
 package com.saturdev.shoppingshare;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.saturdev.shoppingshare.users.Users;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class InfoActivity extends AppCompatActivity {
+
     private static final String TAG = "TAG";
     @SuppressLint("NonConstantResourceId")
     @BindView(R.id.buttonSignIn)
@@ -32,29 +41,75 @@ public class InfoActivity extends AppCompatActivity {
     @BindView(R.id.editTextEmail)
     EditText mEmail;
 
+    Users user = new Users();
+    int maxId;
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("users");
     FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
         ButterKnife.bind(this);
 
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists())
+                    maxId = (int) snapshot.getChildrenCount();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        SharedPreferences mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        @SuppressLint("CommitPrefEdits") SharedPreferences.Editor mEditor = mSharedPreferences.edit();
+
+
         mSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                databaseReference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (!snapshot.exists()) {
+                            String username = mName.getText().toString().trim();
+                            String email = mEmail.getText().toString().trim();
+                            String phone = getIntent().getStringExtra("phonenumber");
+                            saveUserToFirebase(username, email, phone);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
                 updateUser();
-                startActivity(new Intent(InfoActivity.this, HomeActivity.class));
+
+                Intent intent = new Intent(InfoActivity.this, HomeActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
             }
         });
     }
+
 
     private void updateUser() {
         firebaseAuth = FirebaseAuth.getInstance();
         String username = mName.getText().toString().trim();
         String email = mEmail.getText().toString().trim();
+        String phone = getIntent().getStringExtra("phonenumber");
         FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
         if (firebaseUser != null) {
+
+            //Add user to authenticated list
             UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
                     .setDisplayName(username)
                     .build();
@@ -82,4 +137,14 @@ public class InfoActivity extends AppCompatActivity {
             finish();
         }
     }
+
+    //Add user to firebase database
+    private void saveUserToFirebase(String username, String email, String phone) {
+        user.setName(username);
+        user.setEmail(email);
+        user.setPhone(phone);
+        user.setUserId(maxId + 1);
+        databaseReference.child(String.valueOf(maxId + 1)).setValue(user);
+    }
+
 }
--- app/src/main/java/com/saturdev/shoppingshare/MainActivity.java
+++ app/src/main/java/com/saturdev/shoppingshare/MainActivity.java
@@ -1,12 +1,12 @@
 package com.saturdev.shoppingshare;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        SharedPreferences sharedPref = getSharedPreferences("data", MODE_PRIVATE);
+        int number = sharedPref.getInt("isLogged", 0);
+
         mContinueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,5 +31,27 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    mContinueButton.setVisibility(View.GONE);
+                    sleep(1500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
+            }
+        };
+
+
+        if (number == 0) {
+           mContinueButton.setVisibility(View.VISIBLE);
+        } else {
+            timer.start();
+        }
     }
 }
