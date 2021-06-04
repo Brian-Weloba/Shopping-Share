04bd9f568c9b796bfd52107d8c6a906f2a39da83
Brian Weloba <bweloba@gmail.com>
2021-06-04 21:48:20 +0300

    Show cart details

--- app/src/main/java/com/saturdev/shoppingshare/ViewItemActivity.java
+++ app/src/main/java/com/saturdev/shoppingshare/ViewItemActivity.java
@@ -1,14 +1,50 @@
 package com.saturdev.shoppingshare;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ViewItemActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ViewItemActivity extends AppCompatActivity {
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.cartName)
+    TextView cartNameTV;
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.itemPrice)
+    TextView priceTV;
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.itemsInCart)
+    TextView itemsTV;
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.cartDescription)
+    TextView descTV;
+
+
+    DatabaseReference db;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_item);
+        ButterKnife.bind(this);
+        db = FirebaseDatabase.getInstance().getReference();
+
+        String name = getIntent().getStringExtra("name");
+        String description = getIntent().getStringExtra("description");
+        String items = getIntent().getStringExtra("items");
+        String price = getIntent().getStringExtra("price");
+
+        cartNameTV.setText(name);
+        priceTV.setText("KSH "+ price);
+        descTV.setText(description);
+        itemsTV.setText(items);
     }
 }
