eea802fa0f8b3d898e0446cb0705c91a03c7cd15
Brian Weloba <bweloba@gmail.com>
2021-06-07 10:50:29 +0300

    Add items to cart in database

--- app/src/main/java/com/saturdev/shoppingshare/ViewItemActivity.java
+++ app/src/main/java/com/saturdev/shoppingshare/ViewItemActivity.java
@@ -2,12 +2,25 @@ package com.saturdev.shoppingshare;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,9 +38,18 @@ public class ViewItemActivity extends AppCompatActivity {
     @SuppressLint("NonConstantResourceId")
     @BindView(R.id.cartDescription)
     TextView descTV;
+    @BindView(R.id.increaseCart)
+    ImageView increaseIV;
+    @BindView(R.id.reduceCart)
+    ImageView decreaseIV;
+    @BindView(R.id.cartItemCounter)
+    TextView quantityTV;
+    @BindView(R.id.addToBasket)
+    Button addToCartBTN;
 
 
     DatabaseReference db;
+    FirebaseAuth firebaseAuth;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -36,6 +58,7 @@ public class ViewItemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view_item);
         ButterKnife.bind(this);
         db = FirebaseDatabase.getInstance().getReference();
+        firebaseAuth = FirebaseAuth.getInstance();
 
         String name = getIntent().getStringExtra("name");
         String description = getIntent().getStringExtra("description");
@@ -46,5 +69,84 @@ public class ViewItemActivity extends AppCompatActivity {
         priceTV.setText("KSH " + price);
         descTV.setText(description);
         itemsTV.setText(items);
+
+        increaseIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String quantityString = quantityTV.getText().toString();
+                int quantityInt = Integer.parseInt(quantityString);
+                quantityInt++;
+                quantityTV.setText(Integer.toString(quantityInt));
+            }
+        });
+
+        decreaseIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String quantityString = quantityTV.getText().toString();
+                int quantityInt = Integer.parseInt(quantityString);
+                if (quantityInt > 1) {
+                    quantityInt--;
+                } else {
+                    quantityInt = 1;
+                }
+                quantityTV.setText(Integer.toString(quantityInt));
+            }
+        });
+
+        addToCartBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addToCartList();
+            }
+        });
+
+    }
+
+    private void addToCartList() {
+        String saveCurrentTime, saveCurrentDate;
+
+        Calendar calForDate = Calendar.getInstance();
+        SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
+        saveCurrentDate = currentDate.format(calForDate.getTime());
+
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime = currentDate.format(calForDate.getTime());
+
+        final DatabaseReference cartListRef = FirebaseDatabase.getInstance().getReference().child("cart List");
+        HashMap<String, Object> cartMap = new HashMap<>();
+        cartMap.put("name", cartNameTV.getText().toString());
+        cartMap.put("price", priceTV.getText().toString());
+        cartMap.put("description", descTV.getText().toString());
+        cartMap.put("items", itemsTV.getText().toString());
+        cartMap.put("date", saveCurrentDate);
+        cartMap.put("time", saveCurrentTime);
+        cartMap.put("quantity", quantityTV.getText().toString());
+
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        cartListRef.child("User View").child(firebaseUser.getPhoneNumber())
+                .child("Products")
+                .child(cartNameTV.getText().toString())
+                .updateChildren(cartMap)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            cartListRef.child("Admin View").child(firebaseUser.getPhoneNumber())
+                                    .child("Products")
+                                    .child(cartNameTV.getText().toString())
+                                    .updateChildren(cartMap)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()){
+                                                Toast.makeText(ViewItemActivity.this, "Added to cart", Toast.LENGTH_SHORT).show();
+//                                                startActivity(new Intent(ViewItemActivity.this,HomeActivity.class));
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                });
     }
 }
